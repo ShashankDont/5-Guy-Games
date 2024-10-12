2045857e862dc6363188b65f17a2c8b0ea6ee789
Bradley Lashley <bwl23@fsu.edu>
2024-10-11 21:46:09 -0400

    Create Tic Tac Toe script.js
    Stuff to be added to the main Tic Tac Toe

---
+++ tic tac toe/Tic Tac Toe script.js
@@ -0,0 +1,74 @@
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const restartButton = document.getElementById('restartButton');
+let oTurn;
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+  oTurn = false;
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(O_CLASS);
+    cell.removeEventListener('click', handleClick);
+    cell.addEventListener('click', handleClick, { once: true });
+  });
+}
+
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = oTurn ? O_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    setTimeout(() => alert(`${currentClass.toUpperCase()} Wins!`), 10);
+    endGame();
+  } else if (isDraw()) {
+    setTimeout(() => alert('Draw!'), 10);
+    endGame();
+  } else {
+    swapTurns();
+  }
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+  oTurn = !oTurn;
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+  });
+}
+
+function endGame() {
+  cellElements.forEach(cell => {
+    cell.removeEventListener('click', handleClick);
+  });
+}
